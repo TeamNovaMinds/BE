70f47751020515bcbd746dc0425a7b658f9e92fd
MODUGGAGI <kiroro0814@naver.com>
2025-05-03 01:33:47 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/novaminds/gradproj/GradprojApplication.java
@@ -0,0 +1,13 @@
+package novaminds.gradproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/novaminds/gradproj/GradprojApplicationTests.java
@@ -0,0 +1,13 @@
+package novaminds.gradproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
