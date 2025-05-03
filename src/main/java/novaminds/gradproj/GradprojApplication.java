b4a7d20ab7cbf97f2d4706c89fa1c8100495a4d3
MODUGGAGI <kiroro0814@naver.com>
2025-05-03 19:35:26 +0900

    baseEntity 동작하게끔 EnableJpaAuditing 추가

--- src/main/java/novaminds/gradproj/GradprojApplication.java
+++ src/main/java/novaminds/gradproj/GradprojApplication.java
@@ -2,8 +2,10 @@ package novaminds.gradproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GradprojApplication {
 
 	public static void main(String[] args) {
