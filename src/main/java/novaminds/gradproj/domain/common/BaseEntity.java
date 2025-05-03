96c6d3c6e47f4aa38a6a44224882e3730d7c64d7
MODUGGAGI <kiroro0814@naver.com>
2025-05-03 19:35:13 +0900

    baseEntity 각 필드 속성 추가 nullable=false

--- src/main/java/novaminds/gradproj/domain/common/BaseEntity.java
+++ src/main/java/novaminds/gradproj/domain/common/BaseEntity.java
@@ -2,6 +2,7 @@ package novaminds.gradproj.domain.common;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,8 +17,10 @@ import lombok.Getter;
 public abstract class BaseEntity {
 
 	@CreatedDate
+	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdAt;
 
 	@LastModifiedDate
+	@Column(nullable = false)
 	private LocalDateTime updatedAt;
 }
