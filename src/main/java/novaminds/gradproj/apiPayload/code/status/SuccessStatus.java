bbd47c33c9327d3040db8b8811ce9e047a529519
MODUGGAGI <kiroro0814@naver.com>
2025-05-26 01:58:30 +0900

    feat : ApiResponse 응답양식 통일을 위한 패키지 생성

---
+++ src/main/java/novaminds/gradproj/apiPayload/code/status/SuccessStatus.java
@@ -0,0 +1,39 @@
+package novaminds.gradproj.apiPayload.code.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import novaminds.gradproj.apiPayload.code.BaseCode;
+import novaminds.gradproj.apiPayload.code.ReasonDTO;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    // 일반적인 응답
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDTO getReason() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .build();
+    }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .build()
+                ;
+    }
+}
