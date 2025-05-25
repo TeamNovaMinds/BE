485504c9701a637c73199d96932833cd7cd0dd37
MODUGGAGI <kiroro0814@naver.com>
2025-05-26 01:59:03 +0900

    feat : Security 도입으로 메소드들 추가

--- src/main/java/novaminds/gradproj/domain/user/UserRepository.java
+++ src/main/java/novaminds/gradproj/domain/user/UserRepository.java
@@ -2,5 +2,12 @@ package novaminds.gradproj.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsById(String loginId);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByNickname(String nickname);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
