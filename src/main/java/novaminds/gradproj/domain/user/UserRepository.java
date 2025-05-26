f813b70c7f5274498c90c167f253147554814756
MODUGGAGI <kiroro0814@naver.com>
2025-05-26 11:04:01 +0900

    feat : fetch join을 위해서 메소드 추가

--- src/main/java/novaminds/gradproj/domain/user/UserRepository.java
+++ src/main/java/novaminds/gradproj/domain/user/UserRepository.java
@@ -1,13 +1,22 @@
 package novaminds.gradproj.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsById(String loginId);
+
     Optional<User> findByEmail(String email);
+
+    @Query("select u from User u left join fetch u.interestCategories where u.loginId = :loginId")
+    Optional<User> findByLoginIdWithInterestCategories(@Param("loginId") String loginId);
+
     Optional<User> findByNickname(String nickname);
+
     boolean existsByEmail(String email);
+
     boolean existsByNickname(String nickname);
 }
