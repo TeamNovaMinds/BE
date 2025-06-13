a8990765adf8bc2cbeb9a7aec4f3fc61d1dd4536
이찬우 <ichan-u@ichan-uui-MacBookAir.local>
2025-06-13 17:10:07 +0900

    레시피 상세보기 및 댓글기능추가

--- src/main/java/novaminds/gradproj/domain/Recipe/RecipeRepository.java
+++ src/main/java/novaminds/gradproj/domain/Recipe/RecipeRepository.java
@@ -1,6 +1,12 @@
 package novaminds.gradproj.domain.Recipe;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+	@EntityGraph(attributePaths = {"author", "recipeImages"})
+	Optional<Recipe> findById(Long recipeId);
 }
--- src/main/java/novaminds/gradproj/web/dto/recipe/RecipeResponseDTO.java
+++ src/main/java/novaminds/gradproj/web/dto/recipe/RecipeResponseDTO.java
@@ -1,9 +1,20 @@
 package novaminds.gradproj.web.dto.recipe;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import novaminds.gradproj.domain.Recipe.Difficulty;
+import novaminds.gradproj.domain.Recipe.RecipeCategory;
+import novaminds.gradproj.domain.Recipe.RecipeComment;
+import novaminds.gradproj.domain.Recipe.RecipeImage;
+import novaminds.gradproj.domain.Recipe.RecipeIngredient;
+import novaminds.gradproj.domain.Recipe.RecipeOrder;
+import novaminds.gradproj.domain.user.User;
 
 public class RecipeResponseDTO {
 
@@ -15,4 +26,128 @@ public class RecipeResponseDTO {
 		private Long recipeId;
 	}
 
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class RecipeDetailDTO{
+		private Long recipeId;
+		private String title;
+		private String description;
+		private RecipeCategory recipeCategory;
+		private Integer cookingTimeMinutes;
+		private Difficulty difficulty;
+		private Integer servings;
+		private LocalDateTime createdAt;
+		private Integer likeCount;
+		private CommentPreviewDTO commentPreview; // 댓글 미리보기 정보
+		private AuthorDTO author;
+		private List<ImageDTO> recipeImages;
+		private List<IngredientDTO> ingredients;
+		private List<OrderDTO> orders;
+
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class AuthorDTO{
+		private String nickname;
+		private String profileImageUrl;
+
+		public static AuthorDTO from(User user) {
+			return AuthorDTO.builder()
+				.nickname(user.getNickname())
+				.profileImageUrl(user.getProfileImage())
+				.build();
+		}
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class ImageDTO{
+		private String imageUrl;
+		private boolean isMain;
+
+		public static ImageDTO from(RecipeImage recipeImage) {
+			return ImageDTO.builder()
+				.imageUrl(recipeImage.getImageUrl())
+				.isMain(recipeImage.isMain())
+				.build();
+		}
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class IngredientDTO{
+		private String name;
+		private String amount;
+		private String description;
+
+		public static IngredientDTO from(RecipeIngredient recipeIngredient) {
+			return IngredientDTO.builder()
+				.name(recipeIngredient.getIngredient().getIngredientName())
+				.amount(recipeIngredient.getAmount())
+				.build();
+		}
+	}
+
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class OrderDTO{
+		private Integer order;
+		private String description;
+		private String imageUrl;
+
+		public static OrderDTO from(RecipeOrder recipeOrder) {
+			return OrderDTO.builder()
+				.order(recipeOrder.getOrder())
+				.description(recipeOrder.getDescription())
+				.imageUrl(recipeOrder.getImgUrl())
+				.build();
+		}
+	}
+
+	//전체 댓글 더보기 할 때 사용할 dto
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class CommentDTO{
+		private Long commentId;
+		private String content;
+		private LocalDateTime createdAt;
+		private AuthorDTO author;
+		private List<CommentDTO> children;
+
+		public static CommentDTO from(RecipeComment comment) {
+			return CommentDTO.builder()
+				.commentId(comment.getId())
+				.content(comment.getContent())
+				.createdAt(comment.getCreatedAt())
+				.author(AuthorDTO.from(comment.getAuthor()))
+				.children(comment.getChildren().stream()
+					.map(CommentDTO::from)
+					.collect(Collectors.toList()))
+				.build();
+		}
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class CommentPreviewDTO{
+		private long totalCount;
+		private List<CommentDTO> previewComments;
+	}
 }
