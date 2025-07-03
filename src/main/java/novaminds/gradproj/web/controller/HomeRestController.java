3ea12ea8cbc82964fadf6d209110c55328d6b284
MODUGGAGI <kiroro0814@naver.com>
2025-07-04 02:32:04 +0900

    ✨feat: update home response message to provide clearer success indication

--- src/main/java/novaminds/gradproj/web/controller/HomeRestController.java
+++ src/main/java/novaminds/gradproj/web/controller/HomeRestController.java
@@ -4,10 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import novaminds.gradproj.apiPayload.ApiResponse;
-import novaminds.gradproj.domain.user.User;
 import novaminds.gradproj.security.auth.CurrentLoginId;
-import novaminds.gradproj.security.auth.CurrentUser;
-import novaminds.gradproj.service.AuthService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Home", description = "홈 화면 보여주는 API, 현재는 로그인 정보 유지 잘 되나 테스트용!!")
 public class HomeRestController {
 
-    private final AuthService authService;
-
     @GetMapping
     public String home() {
-        return "성공";
+        return "이제는 성공";
     }
 
     @GetMapping("/test")
