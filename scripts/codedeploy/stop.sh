54869f3e0c944176d715f07cb3b94214016a8fd8
MODUGGAGI <kiroro0814@naver.com>
2025-07-26 23:00:10 +0900

    ✨feat: 새로운 배포 버전 적용

--- scripts/codedeploy/stop.sh
+++ scripts/codedeploy/stop.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-echo "CodeDeploy 'ApplicationStop' hook is running successfully!" > /tmp/codedeploy_test_success.txt
-date >> /tmp/codedeploy_test_success.txt
+#echo "CodeDeploy 'ApplicationStop' hook is running successfully!" > /tmp/codedeploy_test_success.txt
+#date >> /tmp/codedeploy_test_success.txt
+echo "🚀 이전 버전의 애플리케이션을 중지합니다..."
+cd /home/ubuntu/deployment
 
-#echo "🚀 이전 버전의 애플리케이션을 중지합니다..."
-#cd /home/ubuntu/deployment
-#
-## docker-compose.yml 파일이 있으면 실행 중인 컨테이너를 중지하고 삭제합니다.
-#if [ -f docker-compose.yml ]; then
-#    docker-compose down --remove-orphans
-#    echo "✅ 기존 컨테이너가 중지 및 삭제되었습니다."
-#else
-#    echo "ℹ️ docker-compose.yml 파일이 없어 중지할 컨테이너가 없습니다."
-#fi
+# docker-compose.yml 파일이 있으면 실행 중인 컨테이너를 중지하고 삭제합니다.
+if [ -f docker-compose.yml ]; then
+    docker-compose down --remove-orphans
+    echo "✅ 기존 컨테이너가 중지 및 삭제되었습니다."
+else
+    echo "ℹ️ docker-compose.yml 파일이 없어 중지할 컨테이너가 없습니다."
+fi
