83a8f313bdf8ef86ef85539f11b522ea57982702
MODUGGAGI <kiroro0814@naver.com>
2025-08-27 16:05:38 +0900

    ✨feat: RefrigeratorSkinImageRepository 추가

---
+++ src/main/java/novaminds/gradproj/domain/refrigerator/repository/RefrigeratorSkinImageRepository.java
@@ -0,0 +1,7 @@
+package novaminds.gradproj.domain.refrigerator.repository;
+
+import novaminds.gradproj.domain.refrigerator.entity.RefrigeratorSkinImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefrigeratorSkinImageRepository extends JpaRepository<RefrigeratorSkinImage, Long> {
+}
