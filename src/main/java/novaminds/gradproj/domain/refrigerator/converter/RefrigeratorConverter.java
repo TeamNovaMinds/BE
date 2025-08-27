e27ea65eacb4149a6cd5848736e2fa723890abc5
MODUGGAGI <kiroro0814@naver.com>
2025-08-27 15:54:37 +0900

    ✨feat: 냉장고 관련 DTO 변환 메소드 추가

--- src/main/java/novaminds/gradproj/domain/refrigerator/converter/RefrigeratorConverter.java
+++ src/main/java/novaminds/gradproj/domain/refrigerator/converter/RefrigeratorConverter.java
@@ -1,4 +1,48 @@
 package novaminds.gradproj.domain.refrigerator.converter;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import novaminds.gradproj.domain.refrigerator.entity.RefrigeratorSkin;
+import novaminds.gradproj.domain.refrigerator.entity.RefrigeratorSkinImage;
+import novaminds.gradproj.domain.refrigerator.web.dto.RefrigeratorResponseDTO;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RefrigeratorConverter {
+
+    public static RefrigeratorResponseDTO.RefrigeratorSkinResponse toRefrigeratorSkinResponse(RefrigeratorSkin refrigeratorSkin) {
+
+        var skinImageUrls = refrigeratorSkin.getRefrigeratorSkinImages().stream()
+                .map(RefrigeratorConverter::toRefrigeratorSkinImageUrlResponse)
+                .toList();
+
+        return RefrigeratorResponseDTO.RefrigeratorSkinResponse.builder()
+                .id(refrigeratorSkin.getId())
+                .name(refrigeratorSkin.getSkinName())
+                .description(refrigeratorSkin.getDescription())
+                .skinImageUrls(skinImageUrls)
+                .build();
+    }
+
+    public static RefrigeratorResponseDTO.RefrigeratorSkinListResponse toRefrigeratorSkinListResponse(RefrigeratorSkin refrigeratorSkin) {
+        // 첫 번째 이미지를 썸네일로 사용
+        String thumbnailUrl = refrigeratorSkin.getRefrigeratorSkinImages().stream()
+                .findFirst()
+                .map(RefrigeratorSkinImage::getImageUrl)
+                .orElse(null);
+
+        return RefrigeratorResponseDTO.RefrigeratorSkinListResponse.builder()
+                .id(refrigeratorSkin.getId())
+                .name(refrigeratorSkin.getSkinName())
+                .description(refrigeratorSkin.getDescription())
+                .price(refrigeratorSkin.getPrice())
+                .thumbnailUrl(thumbnailUrl)
+                .build();
+    }
+
+    private static RefrigeratorResponseDTO.RefrigeratorSkinImageUrlResponse toRefrigeratorSkinImageUrlResponse(RefrigeratorSkinImage skinImage) {
+        return RefrigeratorResponseDTO.RefrigeratorSkinImageUrlResponse.builder()
+                .imageOrder(skinImage.getImageOrder())
+                .ImageUrl(skinImage.getImageUrl())
+                .build();
+    }
 }
