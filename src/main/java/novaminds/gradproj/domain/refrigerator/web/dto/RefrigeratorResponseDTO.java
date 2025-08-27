c537f5cf935044b4e39a5924563f859d6c56a48b
MODUGGAGI <kiroro0814@naver.com>
2025-08-27 15:55:55 +0900

    ✨feat: 냉장고 스킨 관련 응답 DTO

---
+++ src/main/java/novaminds/gradproj/domain/refrigerator/web/dto/RefrigeratorResponseDTO.java
@@ -0,0 +1,54 @@
+package novaminds.gradproj.domain.refrigerator.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class RefrigeratorResponseDTO {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class RefrigeratorSkinResponse {
+        private Long id;
+        private String name;
+        private String description;
+        private String imageUrl;
+        private List<RefrigeratorSkinImageUrlResponse> skinImageUrls;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class RefrigeratorSkinImageUrlResponse {
+        private int imageOrder;
+        private String ImageUrl;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class RefrigeratorSkinsPageResponse {
+        private List<RefrigeratorSkinListResponse> skins;
+        private Long nextCursor;
+        private boolean hasNext;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class RefrigeratorSkinListResponse {
+        private Long id;
+        private String name;
+        private String description;
+        private Integer price;
+        private String thumbnailUrl;
+    }
+}
