948a568e266175327018a7f51ec52b29162fcdaa
MODUGGAGI <kiroro0814@naver.com>
2025-08-27 17:19:06 +0900

    ✨feat: 냉장고 스킨 등록, 구매 메소드 추가

---
+++ src/main/java/novaminds/gradproj/domain/refrigerator/service/command/RefrigeratorSkinCommandService.java
@@ -0,0 +1,103 @@
+package novaminds.gradproj.domain.refrigerator.service.command;
+
+import lombok.RequiredArgsConstructor;
+import novaminds.gradproj.apiPayload.code.status.ErrorStatus;
+import novaminds.gradproj.apiPayload.exception.GeneralException;
+import novaminds.gradproj.domain.member.entity.Member;
+import novaminds.gradproj.domain.refrigerator.entity.MemberRefrigeratorSkin;
+import novaminds.gradproj.domain.refrigerator.repository.MemberRefrigeratorSkinRepository;
+import novaminds.gradproj.domain.member.repository.MemberRepository;
+import novaminds.gradproj.domain.refrigerator.converter.RefrigeratorConverter;
+import novaminds.gradproj.domain.refrigerator.entity.RefrigeratorSkin;
+import novaminds.gradproj.domain.refrigerator.entity.RefrigeratorSkinImage;
+import novaminds.gradproj.domain.refrigerator.repository.RefrigeratorSkinImageRepository;
+import novaminds.gradproj.domain.refrigerator.repository.RefrigeratorSkinRepository;
+import novaminds.gradproj.domain.refrigerator.web.dto.RefrigeratorRequestDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RefrigeratorSkinCommandService {
+
+    private final MemberRepository memberRepository;
+    private final RefrigeratorSkinRepository refrigeratorSkinRepository;
+    private final RefrigeratorSkinImageRepository refrigeratorSkinImageRepository;
+    private final MemberRefrigeratorSkinRepository memberRefrigeratorSkinRepository;
+
+    public Long registerRefrigeratorSkin(String memberId, RefrigeratorRequestDTO.RefrigeratorSkinCreateRequest request) {
+
+        // 관리자인지 권한 확인
+        if (!memberRepository.isAdminByLoginId(memberId)) {
+            throw new GeneralException(ErrorStatus.MEMBER_NOT_ADMIN);
+        }
+
+        // 냉장고 스킨 생성
+        RefrigeratorSkin refrigeratorSkin = RefrigeratorSkin.builder()
+                .skinName(request.getSkinName())
+                .description(request.getDescription())
+                .price(request.getPrice())
+                .build();
+
+        // 저장한 스킨 가져오기
+        RefrigeratorSkin savedRefrigeratorSkin = refrigeratorSkinRepository.save(refrigeratorSkin);
+
+        // 스킨 이미지 url 가져오기
+        List<String> imageUrls = request.getImageUrls();
+
+        // 가져온 이미지 url로 스킨 이미지 엔티티 생성
+        List<RefrigeratorSkinImage> skinImages = IntStream.range(0, imageUrls.size())
+                .mapToObj(i -> RefrigeratorSkinImage.builder()
+                        .refrigeratorSkin(savedRefrigeratorSkin)
+                        .imageUrl(imageUrls.get(i))
+                        .imageOrder(i + 1)
+                        .build())
+                .toList();
+
+        // 생성된 스킨 이미지 엔티티 모두 저장
+        refrigeratorSkinImageRepository.saveAll(skinImages);
+
+        // 스킨 이미지 엔티티와 스킨과의 연관관계 설정
+        skinImages.forEach(savedRefrigeratorSkin::addRefrigeratorSkinImage);
+
+        return savedRefrigeratorSkin.getId();
+    }
+
+    public void purchaseRefrigeratorSkin(String memberId, Long skinId) {
+
+        // 해당 냉장고 스킨의 구매 여부 확인
+        boolean owned = memberRefrigeratorSkinRepository.existsByMemberLoginIdAndSkinId(memberId, skinId);
+        if (owned) {
+            throw new GeneralException(ErrorStatus.REFRIGERATOR_SKIN_ALREADY_OWNED);
+        }
+
+        // 회원 조회
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.MEMBER_NOT_FOUND));
+
+        // 스킨 조회
+        RefrigeratorSkin refrigeratorSkin = refrigeratorSkinRepository.findById(skinId)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.REFRIGERATOR_SKIN_NOT_FOUND));
+
+        // 포인트 부족한지 점검
+        if (member.getPoint() < refrigeratorSkin.getPrice()) {
+            throw new GeneralException(ErrorStatus.INSUFFICIENT_POINTS);
+        }
+
+        // 회원과 스킨 연결
+        MemberRefrigeratorSkin memberRefrigeratorSkin = RefrigeratorConverter.toMemberRefrigeratorSkin(member, refrigeratorSkin);
+
+        // 회원의 냉장고 구매 관계 저장
+        memberRefrigeratorSkinRepository.save(memberRefrigeratorSkin);
+
+        // 양방향 연관관계 설정
+        member.getPurchasedSkins().add(memberRefrigeratorSkin);
+
+        // 냉장고 스킨의 가격 만큼 포인트 차감
+        member.usePoint(refrigeratorSkin.getPrice());
+    }
+}
