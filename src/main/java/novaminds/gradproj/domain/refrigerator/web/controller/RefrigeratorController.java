71040cbd92af94cb03c5c44ff677d2ca408afa9d
MODUGGAGI <kiroro0814@naver.com>
2025-08-27 17:20:17 +0900

    ✨feat: 냉장고 스킨 관련 메소드 및 서비스 클래스 이름 변경, 스킨 구매 메소드 추가

--- src/main/java/novaminds/gradproj/domain/refrigerator/web/controller/RefrigeratorController.java
+++ src/main/java/novaminds/gradproj/domain/refrigerator/web/controller/RefrigeratorController.java
@@ -6,8 +6,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import novaminds.gradproj.apiPayload.ApiResponse;
 import novaminds.gradproj.domain.member.service.security.auth.CurrentLoginId;
-import novaminds.gradproj.domain.refrigerator.service.command.RefrigeratorCommandService;
-import novaminds.gradproj.domain.refrigerator.service.query.RefrigeratorQueryService;
+import novaminds.gradproj.domain.refrigerator.service.command.RefrigeratorSkinCommandService;
+import novaminds.gradproj.domain.refrigerator.service.query.RefrigeratorSkinQueryService;
 import novaminds.gradproj.domain.refrigerator.web.dto.RefrigeratorRequestDTO;
 import novaminds.gradproj.domain.refrigerator.web.dto.RefrigeratorResponseDTO;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "냉장고 관련 API", description = "냉장고에 아이템 등록, 냉장고 스킨 등록, 구매, 조회 등등을 처리하는 API 입니다.")
 public class RefrigeratorController {
 
-    private final RefrigeratorCommandService refrigeratorCommandService;
-    private final RefrigeratorQueryService refrigeratorQueryService;
+    private final RefrigeratorSkinCommandService refrigeratorSkinCommandService;
+    private final RefrigeratorSkinQueryService refrigeratorSkinQueryService;
 
     @Operation(summary = "냉장고 스킨 등록", description = "냉장고의 스킨을 등록합니다.")
     @ApiResponses({
@@ -30,7 +30,7 @@ public class RefrigeratorController {
             @CurrentLoginId String memberId,
             @RequestBody RefrigeratorRequestDTO.RefrigeratorSkinCreateRequest request
     ) {
-        return ApiResponse.onSuccess(refrigeratorCommandService.registerRefrigeratorSkin(memberId, request));
+        return ApiResponse.onSuccess(refrigeratorSkinCommandService.registerRefrigeratorSkin(memberId, request));
     }
 
     @Operation(summary = "냉장고 스킨 리스트 조회", description = "냉장고 스킨을 리스트로 조회합니다..")
@@ -42,7 +42,7 @@ public class RefrigeratorController {
             @CurrentLoginId String memberId,
             @RequestParam(required = false) Long cursorId
     ) {
-        return ApiResponse.onSuccess(refrigeratorQueryService.getRefrigeratorSkins(cursorId, memberId));
+        return ApiResponse.onSuccess(refrigeratorSkinQueryService.getRefrigeratorSkins(cursorId, memberId));
     }
 
     @Operation(summary = "냉장고 스킨 상세 조회", description = "특정 냉장고 스킨을 상세조회합니다.")
@@ -54,6 +54,23 @@ public class RefrigeratorController {
             @CurrentLoginId String memberId,
             @PathVariable Long skinId
     ) {
-        return ApiResponse.onSuccess(refrigeratorQueryService.getRefrigeratorSkin(skinId, memberId));
+        return ApiResponse.onSuccess(refrigeratorSkinQueryService.getRefrigeratorSkin(skinId, memberId));
+    }
+
+    @Operation(summary = "특정 냉장고 스킨 구매", description = "냉장고의 스킨을 구매합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER4001", description = "실패, 회원 존재 X"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "SKIN4002", description = "실패, 스킨 존재 X"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "SKIN4003", description = "실패, 스킨 이미 구매"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "POINT4001", description = "실패, 포인트 부족"),
+    })
+    @PostMapping("/skins/{skinId}")
+    public ApiResponse<String> purchaseRefrigeratorSkin(
+            @CurrentLoginId String memberId,
+            @PathVariable Long skinId
+    ) {
+        refrigeratorSkinCommandService.purchaseRefrigeratorSkin(memberId, skinId);
+        return ApiResponse.onSuccess("냉장고 스킨 구매가 완료되었습니다.");
     }
 }
