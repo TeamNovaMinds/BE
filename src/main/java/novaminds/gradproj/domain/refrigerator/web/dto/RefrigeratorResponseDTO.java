5d8eba75af5c2932d773f72a283eb4c16eb31558
MODUGGAGI <kiroro0814@naver.com>
2025-08-27 17:19:27 +0900

    ♻️refactor: 중복된 imageUrl 삭제

--- src/main/java/novaminds/gradproj/domain/refrigerator/web/dto/RefrigeratorResponseDTO.java
+++ src/main/java/novaminds/gradproj/domain/refrigerator/web/dto/RefrigeratorResponseDTO.java
@@ -17,7 +17,6 @@ public class RefrigeratorResponseDTO {
         private Long id;
         private String name;
         private String description;
-        private String imageUrl;
         private List<RefrigeratorSkinImageUrlResponse> skinImageUrls;
         private boolean owned;
     }
