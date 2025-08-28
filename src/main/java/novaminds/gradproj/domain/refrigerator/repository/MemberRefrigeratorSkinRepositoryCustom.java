f16f9f3f87de266010522a9b54b4379e47b341d5
MODUGGAGI <kiroro0814@naver.com>
2025-08-28 14:43:00 +0900

    ✨feat: 회원 로그인 ID와 스킨 ID 목록으로 냉장고 스킨 배치 조회 메소드 추가

--- src/main/java/novaminds/gradproj/domain/refrigerator/repository/MemberRefrigeratorSkinRepositoryCustom.java
+++ src/main/java/novaminds/gradproj/domain/refrigerator/repository/MemberRefrigeratorSkinRepositoryCustom.java
@@ -3,7 +3,10 @@ package novaminds.gradproj.domain.refrigerator.repository;
 import novaminds.gradproj.domain.refrigerator.entity.MemberRefrigeratorSkin;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MemberRefrigeratorSkinRepositoryCustom {
     List<MemberRefrigeratorSkin> findOwnedSkinsWithCursor(String memberId, Long cursorId, int pageSize);
+    
+    Map<Long, MemberRefrigeratorSkin> findByMemberLoginIdAndSkinIds(String memberId, List<Long> skinIds);
 }
