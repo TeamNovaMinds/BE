d893ceaf789f3285bda7ff905077a0130192e4f6
MODUGGAGI <kiroro0814@naver.com>
2025-08-29 00:03:27 +0900

    ♻️refactor: AuthRestController에서 로그아웃 메서드에 HttpServletRequest 추가

--- src/main/java/novaminds/gradproj/domain/member/web/controller/AuthRestController.java
+++ src/main/java/novaminds/gradproj/domain/member/web/controller/AuthRestController.java
@@ -2,6 +2,7 @@ package novaminds.gradproj.domain.member.web.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import novaminds.gradproj.apiPayload.ApiResponse;
 import novaminds.gradproj.domain.member.entity.Member;
 import novaminds.gradproj.domain.member.service.security.auth.CurrentUser;
-import novaminds.gradproj.domain.member.service.security.oauth2.CustomOAuth2UserService;
 import novaminds.gradproj.domain.member.service.AuthService;
 import novaminds.gradproj.domain.member.web.dto.AuthRequest;
 import novaminds.gradproj.domain.member.web.dto.AuthResponse;
@@ -25,8 +25,6 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @Tag(name = "인증", description = "로그인/회원가입 관련 API")
 public class AuthRestController {
-
-    //테스트용 주석
 
     private final AuthService authService;
 
@@ -107,9 +105,9 @@ public class AuthRestController {
     }
 
     @PostMapping("/logout")
-    public ApiResponse<String> logout(HttpServletResponse response) {
+    public ApiResponse<String> logout(HttpServletRequest request, HttpServletResponse response) {
         log.info("🔸 [API 호출] 로그아웃");
-        authService.logout(response);
+        authService.logout(request, response);
         return ApiResponse.onSuccess("로그아웃이 완료되었습니다.");
     }
 
