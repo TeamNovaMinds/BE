aa8efb9c297f70fda0cc598e398020e87361ba6c
MODUGGAGI <kiroro0814@naver.com>
2025-08-28 17:13:06 +0900

    ✨feat: redis 자동 설정 제거

--- src/main/java/novaminds/gradproj/GradprojApplication.java
+++ src/main/java/novaminds/gradproj/GradprojApplication.java
@@ -3,10 +3,12 @@ package novaminds.gradproj;
 import io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(
 		exclude = {
+                RedisRepositoriesAutoConfiguration.class,
 				S3AutoConfiguration.class
 		}
 )
