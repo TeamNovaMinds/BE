aba5c7710e6792d7aa8cae7f201b045a971822c0
MODUGGAGI <kiroro0814@naver.com>
2025-08-29 02:19:00 +0900

    ♻️refactor: 불필요한 import 제거

--- src/main/java/novaminds/gradproj/domain/member/service/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/novaminds/gradproj/domain/member/service/security/jwt/JwtAuthenticationFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import novaminds.gradproj.config.properties.JwtProperties;
 import novaminds.gradproj.domain.member.service.security.auth.AuthRedisService;
 import novaminds.gradproj.domain.member.service.security.auth.AuthenticationHelper;
 import novaminds.gradproj.domain.member.service.security.auth.PrincipalDetails;
