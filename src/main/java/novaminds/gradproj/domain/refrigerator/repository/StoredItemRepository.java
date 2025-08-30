15ed4cfe3af7fa8be9f121df630e5c2938b580e6
MODUGGAGI <kiroro0814@naver.com>
2025-08-30 16:17:52 +0900

    ♻️refactor: 냉장고, 재료, 보관 방법으로 StoredItem 조회 메소드 추가

--- src/main/java/novaminds/gradproj/domain/refrigerator/repository/StoredItemRepository.java
+++ src/main/java/novaminds/gradproj/domain/refrigerator/repository/StoredItemRepository.java
@@ -3,7 +3,11 @@ package novaminds.gradproj.domain.refrigerator.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import novaminds.gradproj.domain.refrigerator.entity.StoredItem;
+import novaminds.gradproj.domain.refrigerator.entity.StorageType;
+
+import java.util.Optional;
 
 public interface StoredItemRepository extends JpaRepository<StoredItem, Long> {
 
+    Optional<StoredItem> findByRefrigeratorIdAndIngredientIdAndStorageType(Long refrigeratorId, Long ingredientId, StorageType storageType);
 }
