66d185a5a274bac3bd511faf393f07d03337454c
MODUGGAGI <kiroro0814@naver.com>
2025-08-30 21:38:57 +0900

    ♻️refactor: 쓰이지 않는 에러 코드 제거

--- src/main/java/novaminds/gradproj/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/novaminds/gradproj/apiPayload/code/status/ErrorStatus.java
@@ -71,7 +71,6 @@ public enum ErrorStatus implements BaseErrorCode {
 
     //냉장고 관련 에러
     REFRIGERATOR_NOT_FOUND(HttpStatus.NOT_FOUND, "REFRIGERATOR401", "냉장고를 찾을 수 없습니다."),
-    INVALID_STORAGE_TYPE(HttpStatus.BAD_REQUEST, "REFRIGERATOR402", "유효하지 않은 보관 방식입니다."),
     STORED_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "REFRIGERATOR403", "냉장고 재료를 찾을 수 없습니다."),
     STORED_ITEM_ACCESS_DENIED(HttpStatus.FORBIDDEN, "REFRIGERATOR404", "해당 냉장고 재료에 대한 접근 권한이 없습니다."),
 
