5f830e3016dbb40027b2f97564970e8d878213d4
MODUGGAGI <kiroro0814@naver.com>
2025-08-31 17:21:08 +0900

    ♻️refactor: 필요없는 업데이트용 DTO 제거

--- src/main/java/novaminds/gradproj/domain/recipe/web/dto/RecipeRequestDTO.java
+++ src/main/java/novaminds/gradproj/domain/recipe/web/dto/RecipeRequestDTO.java
@@ -67,21 +67,4 @@ public class RecipeRequestDTO {
 		private String description;
 		private String stepImage;
 	}
-
-	@Builder
-	@Getter
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class RecipeUpdateDTO{
-		private String title;
-		private String description;
-		private RecipeCategory recipeCategory;
-		private Integer cookingTimeMinutes;
-		private Difficulty difficulty;
-		private Integer servings;
-
-		private List<RecipeIngredientDTO> ingredients;
-		private List<RecipeOrderDTO> orders;
-		private List<Long> deletedRecipeImages;
-	}
 }
