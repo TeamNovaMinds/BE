d399b854a88b0b425c4d0d3863352b3113d26a43
MODUGGAGI <kiroro0814@naver.com>
2025-09-01 21:56:06 +0900

    ♻️refactor: 특정 레시피의 이미지들을 순서대로 조회하는 메소드 추가

--- src/main/java/novaminds/gradproj/domain/recipe/repository/RecipeImageRepository.java
+++ src/main/java/novaminds/gradproj/domain/recipe/repository/RecipeImageRepository.java
@@ -2,8 +2,12 @@ package novaminds.gradproj.domain.recipe.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 import novaminds.gradproj.domain.recipe.entity.RecipeImage;
 
 public interface RecipeImageRepository extends JpaRepository<RecipeImage, Long> {
 
+    // 특정 레시피의 이미지들을 순서대로 조회
+    List<RecipeImage> findByRecipeIdOrderByImageOrder(Long recipeId);
 }
