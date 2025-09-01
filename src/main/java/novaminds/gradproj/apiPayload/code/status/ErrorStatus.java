5acbfcbab4cfef4a2c85c436c6797a9d1b996d9a
MODUGGAGI <kiroro0814@naver.com>
2025-09-01 23:26:53 +0900

    ♻️refactor: 레시피 관련 에러 코드 수정

--- src/main/java/novaminds/gradproj/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/novaminds/gradproj/apiPayload/code/status/ErrorStatus.java
@@ -78,8 +78,7 @@ public enum ErrorStatus implements BaseErrorCode {
 
     //레시피 관련 에러
     RECIPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECIPE401", "해당 레시피를 찾을 수 없습니다."),
-    RECIPE_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "BOOK_STORY_402", "레시피 수정/삭제 권한이 없습니다."),
-    RECIPE_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, "RECIPE402", "레시피 삭제 권한이 없습니다."),
+    RECIPE_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "RECIPE_402", "레시피 수정/삭제 권한이 없습니다."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT401", "해당 댓글을 찾을 수 없습니다."),
     COMMENT_NOT_MATCH_RECIPE(HttpStatus.BAD_REQUEST, "COMMENT402", "댓글이 해당 레시피에 속하지 않습니다."),
     COMMENT_DEPTH_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "COMMENT403", "댓글은 2단계까지만 허용됩니다."),
