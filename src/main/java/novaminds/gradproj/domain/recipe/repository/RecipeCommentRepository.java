3d9e550bfd5b18fe3fd7dd8919970b49366b8027
MODUGGAGI <kiroro0814@naver.com>
2025-09-03 17:10:34 +0900

    ♻️refactor: 댓글 조회 쿼리의 정렬 방식 및 가독성 향상

--- src/main/java/novaminds/gradproj/domain/recipe/repository/RecipeCommentRepository.java
+++ src/main/java/novaminds/gradproj/domain/recipe/repository/RecipeCommentRepository.java
@@ -13,9 +13,8 @@ import novaminds.gradproj.domain.recipe.entity.RecipeComment;
 
 public interface RecipeCommentRepository extends JpaRepository<RecipeComment, Long>, RecipeCommentRepositoryCustom {
 
-    //댓글 더보기 누르기 전 미리보기로 보여줄 댓글 3개 리스트 (대댓글까지 fetch join)
-    @EntityGraph(attributePaths = "children")
-    List<RecipeComment> findTop3ByRecipeIdAndParentCommentIsNullOrderByCreatedAtDesc(Long recipeId);
+    //댓글 더보기 누르기 전 미리보기로 보여줄 댓글 3개 리스트
+    List<RecipeComment> findTop3ByRecipeIdAndParentCommentIsNullOrderByCreatedAtAsc(Long recipeId);
 
     //특정 레시피의 총 댓글 수 조회 (부모 댓글 + 대댓글)
     @Query("SELECT COUNT(c) " +
@@ -27,15 +26,23 @@ public interface RecipeCommentRepository extends JpaRepository<RecipeComment, Lo
     boolean existsByIdAndRecipeId(Long id, Long recipeId);
 
     // 여러 레시피 ID에 대한 댓글 수를 배치 조회
-    @Query("SELECT c.recipe.id, COUNT(c) " +
-            "FROM RecipeComment c " +
-            "WHERE c.recipe.id IN :recipeIds " +
-            "GROUP BY c.recipe.id")
+    @Query("""
+            SELECT
+            c.recipe.id AS recipeId,
+            COUNT(c) AS cnt
+            FROM RecipeComment c
+            WHERE c.recipe.id IN :recipeIds
+            GROUP BY c.recipe.id
+            """)
     List<RecipeCommentCount> countCommentsByRecipeIds(@Param("recipeIds") List<Long> recipeIds);
 
     // 댓글 ID와 작성자 ID를 함께 조회 - c.author.loginId 에서 N+1 문제 발생 XXX -> JPA가 효율적인 JOIN으로 문제 해결
-    @Query("SELECT c.id as id, c.author.loginId as authorId " +
-            "FROM RecipeComment c " +
-            "WHERE c.id IN :commentIds")
+    @Query("""
+            SELECT
+            c.id AS id,
+            c.author.loginId AS authorId
+            FROM RecipeComment c
+            WHERE c.id IN :commentIds
+            """)
     List<CommentAuthorInfo> findCommentIdAndAuthorId(@Param("commentIds") List<Long> commentIds);
 }
