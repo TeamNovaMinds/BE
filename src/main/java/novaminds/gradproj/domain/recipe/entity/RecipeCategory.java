31cc25b030716a5cd2152e1a413d3842b9ac4ae5
MODUGGAGI <kiroro0814@naver.com>
2025-09-03 16:34:34 +0900

    ♻️refactor: 레시피 카테고리 리스트 9개로 압축

--- src/main/java/novaminds/gradproj/domain/recipe/entity/RecipeCategory.java
+++ src/main/java/novaminds/gradproj/domain/recipe/entity/RecipeCategory.java
@@ -6,28 +6,17 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum RecipeCategory {
-    KOREAN("한식"),
-    CHINESE("중식"),
-    JAPANESE("일식"),
-    WESTERN("양식"),
-    ASIAN("아시안"),
-    DESSERT("디저트"),
-    SALAD("샐러드"),
-    SOUP("국/찌개"),
-    SIDE_DISH("반찬"),
-    NOODLE("면요리"),
-    RICE("밥요리"),
-    BREAD("빵/베이커리"),
-    DRINK("음료"),
-    SNACK("간식"),
-    DIET("다이어트"),
-    VEGAN("비건"),
-    QUICK("간편요리"),
-    PARTY("파티요리"),
-    KIDS("아이간식"),
-    OTHER("기타");
 
-    //TODO : 편의점 음식, 술 종류 추가 필요
+    KOREAN("한식"),       // 김치찌개, 불고기
+    CHINESE("중식"),      // 짜장면, 마라탕
+    JAPANESE("일식"),     // 초밥, 돈까스
+    WESTERN("양식"),      // 파스타, 스테이크
+    ASIAN("아시안"),      // 태국, 베트남 요리
+    DESSERT("디저트"),    // 케이크, 푸딩
+    BAKERY("베이커리"),   // 빵, 쿠키
+    SNACK("간식"),        // 떡볶이, 튀김
+    DRINK("음료/술")      // 칵테일, 전통주, 음료
+    ;
 
     private final String koreanName;
 }
