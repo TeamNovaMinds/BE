4dad8b9a364355651b577950c8ce5a0a25a6e265
MODUGGAGI <kiroro0814@naver.com>
2025-09-04 12:03:08 +0900

    ✨feat: 내 냉장고의 재료 개수를 반환하는 조회 메소드 추가

--- src/main/java/novaminds/gradproj/domain/refrigerator/repository/StoredItemRepository.java
+++ src/main/java/novaminds/gradproj/domain/refrigerator/repository/StoredItemRepository.java
@@ -1,13 +1,26 @@
 package novaminds.gradproj.domain.refrigerator.repository;
 
+import novaminds.gradproj.domain.refrigerator.repository.projection.StorageTypeCount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import novaminds.gradproj.domain.refrigerator.entity.StoredItem;
 import novaminds.gradproj.domain.refrigerator.entity.StorageType;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface StoredItemRepository extends JpaRepository<StoredItem, Long>, StoredItemRepositoryCustom {
 
     Optional<StoredItem> findByRefrigeratorIdAndIngredientIdAndStorageType(Long refrigeratorId, Long ingredientId, StorageType storageType);
+
+    @Query("""
+      SELECT
+          COALESCE(SUM(CASE WHEN s.storageType = 'REFRIGERATOR' THEN 1 ELSE 0 END), 0) AS refrigeratorCount,
+          COALESCE(SUM(CASE WHEN s.storageType = 'FREEZER' THEN 1 ELSE 0 END), 0) AS freezerCount,
+          COALESCE(SUM(CASE WHEN s.storageType = 'ROOM_TEMPERATURE' THEN 1 ELSE 0 END), 0) AS roomTempCount
+      FROM StoredItem s
+      WHERE s.refrigerator.id = :refrigeratorId
+      """)
+    StorageTypeCount countByStorageTypes(@Param("refrigeratorId") Long refrigeratorId);
 }
