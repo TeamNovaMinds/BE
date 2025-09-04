ee09578fe2bc1c6c8a08e7a1ac4c25dd6e469c28
MODUGGAGI <kiroro0814@naver.com>
2025-09-04 12:03:19 +0900

    ✨feat: 내 냉장고의 재료 개수를 담을 인터페이스 프로젝션 추가

---
+++ src/main/java/novaminds/gradproj/domain/refrigerator/repository/projection/StorageTypeCount.java
@@ -0,0 +1,8 @@
+package novaminds.gradproj.domain.refrigerator.repository.projection;
+
+public interface StorageTypeCount {
+    Long getRefrigeratorCount();
+    Long getFreezerCount();
+    Long getRoomTempCount();
+}
+
