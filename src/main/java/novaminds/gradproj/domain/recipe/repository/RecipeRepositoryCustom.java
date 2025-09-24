7171e077cdb7037117ad6e3830a82da51aad7845
MODUGGAGI <kiroro0814@naver.com>
2025-09-24 14:06:58 +0900

    ✨feat: 레시피 조회 기능에 keyword 파라미터 추가

--- src/main/java/novaminds/gradproj/domain/recipe/repository/RecipeRepositoryCustom.java
+++ src/main/java/novaminds/gradproj/domain/recipe/repository/RecipeRepositoryCustom.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface RecipeRepositoryCustom {
     
-    List<Recipe> findRecipes(RecipeCategory category, Long cursorId, int pageSize);
+    List<Recipe> findRecipes(RecipeCategory category, String keyword, Long cursorId, int pageSize);
     
     // 재료 ID로 레시피 조회 + 페이징 (좋아요 순 정렬)
     List<Recipe> findRecipesByIngredientIds(List<Long> ingredientIds, Long cursorId, int pageSize);
